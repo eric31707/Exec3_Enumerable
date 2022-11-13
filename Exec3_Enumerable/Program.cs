b1fae1f88b4c82e9931efe5a3d1b72cd8a1f2a0c
eric31707 <eric31707@DESKTOP-95TUUP6>
2022-11-14 00:16:48 +0800

    加入專案檔案。

---
+++ Exec3_Enumerable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec3_Enumerable
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int>numbers=Enumerable.Range(1,10);
+			foreach (int item in numbers)
+			{
+				Console.Write(item+"\r\n");
+			}
+			Console.WriteLine("結束");
+			IEnumerable<int> numbers1 = Enumerable.Range(10, 6);
+			foreach (int item1 in numbers1)
+			{
+				Console.Write(item1+"\r\n");
+			}
+		}
+	}
+}
